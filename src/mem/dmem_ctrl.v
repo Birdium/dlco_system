d2af4383eb7a19109824ea83c3d418414154298b
Birdium <bibishu2001@163.com>
2022-06-03 22:16:25 +0800

    avoid latch

--- src/mem/dmem_ctrl.v
+++ src/mem/dmem_ctrl.v
@@ -23,35 +23,43 @@ dmem my_mem(
 );
 
 always @(*) begin
-	case(memop[1:0])
-		2'b00: byteena_a_t = 4'b0001;
-		2'b01: byteena_a_t = 4'b0011;
-		2'b10: byteena_a_t = 4'b1111;
-		default: byteena_a_t = 4'b0000;
-	endcase
-	case(addr[1:0])
-		2'b00: begin datain_t = datain; byteena_a = byteena_a_t; end
-		2'b01: begin datain_t = {datain[23:0], 8'b0}; byteena_a = {byteena_a_t[2:0], 1'b0}; end
-		2'b10: begin datain_t = {datain[15:0], 16'b0}; byteena_a = {byteena_a_t[1:0], 2'b0}; end
-		2'b11: begin datain_t = {datain[7:0], 24'b0}; byteena_a = {byteena_a_t[0], 3'b0}; end
-		default: begin datain_t = datain; byteena_a = byteena_a_t; end
-	endcase
+	if (we) begin
+		case(memop[1:0])
+			2'b00: byteena_a_t = 4'b0001;
+			2'b01: byteena_a_t = 4'b0011;
+			2'b10: byteena_a_t = 4'b1111;
+			default: byteena_a_t = 4'b0000;
+		endcase
+		case(addr[1:0])
+			2'b00: begin datain_t = datain; byteena_a = byteena_a_t; end
+			2'b01: begin datain_t = {datain[23:0], 8'b0}; byteena_a = {byteena_a_t[2:0], 1'b0}; end
+			2'b10: begin datain_t = {datain[15:0], 16'b0}; byteena_a = {byteena_a_t[1:0], 2'b0}; end
+			2'b11: begin datain_t = {datain[7:0], 24'b0}; byteena_a = {byteena_a_t[0], 3'b0}; end
+			default: begin datain_t = 32'b0; byteena_a = 4'b0; end
+		endcase
+	end
+	else begin
+		byteena_a_t = 4'b0;
+		byteena_a = 4'b0;
+		datain_t = 32'b0;
+	end
 end
 
-always @ (*)
-begin
-	case (memop) 
+always @(*) begin
+	case (memop)
 		3'b000: 
 			case (addr[1:0])
 				2'b00: dataout = {{24{q[7]}}, q[7:0]};
 				2'b01: dataout = {{24{q[15]}}, q[15:8]};
 				2'b10: dataout = {{24{q[23]}}, q[23:16]};
 				2'b11: dataout = {{24{q[31]}}, q[31:24]};
+				default: dataout = 32'b0;
 			endcase
 		3'b001: 
 			case (addr[1])
 				1'b0: dataout = {{16{q[15]}}, q[15:0]};
 				1'b1: dataout = {{16{q[31]}}, q[31:16]};
+				default: dataout = 32'b0;
 			endcase
 		3'b010: dataout = q;
 		3'b100: 
@@ -60,12 +68,16 @@ begin
 				2'b01: dataout = {24'b0, q[15:8]};
 				2'b10: dataout = {24'b0, q[23:16]};
 				2'b11: dataout = {24'b0, q[31:24]};
+				default: dataout = 32'b0;
 			endcase
 		3'b101: 
 			case (addr[1])
 				1'b0: dataout = {16'b0, q[15:0]};
 				1'b1: dataout = {16'b0, q[31:16]};
+				default: dataout = 32'b0;
 			endcase
+		default:
+			dataout = 32'b0;
 	endcase
 end
 
