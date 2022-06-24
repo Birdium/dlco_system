830d1e7f9151a9410cc8ab160b3eb2fefd0cf20b
jpwang <jpwang@smail.nju.edu.cn>
2022-06-24 11:42:59 +0800

    implement 'cmd not found'

--- src/software/main.c
+++ src/software/main.c
@@ -15,14 +15,18 @@ void entry() {
 }
 
 // + sizeof(#str) 是为了跳过命令 "cmd args" 的前缀，把 args 作为参数传入 exec_##str
-#define CMD_EXEC(str) if (!strncmp(cmd, #str, sizeof(#str))) exec_##str(cmd + sizeof(#str));
+#define CMD_EXEC(str) if (!strncmp(cmd, #str, sizeof(#str))) { \
+    exec_##str(cmd + sizeof(#str)); \
+    return 0; }
 #define CMD_DEF( str) void exec_##str(const char *cmd);
 #define COMMANDS(_) _(echo) _(fib) _(eval)
 
 COMMANDS(CMD_DEF)
 
-void exec(const char *cmd) {
+int exec(const char *cmd) {
     COMMANDS(CMD_EXEC);
+    printf("%s: command not found\n", cmd);
+    return 1;
 }
 
 int main() {
